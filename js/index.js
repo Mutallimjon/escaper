15d35f06f5922546beee28f623ff7a8fb43ae8bc
Mutallimjon <68152707+Mutallimjon@users.noreply.github.com>
2022-03-02 14:38:43 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let burgerMenu = document.getElementById('burger_menu')
+let burgerBtn = document.getElementById('burger_btn')
+
+burgerBtn.addEventListener('click',function(){
+    burgerMenu.classList.toggle('active')
+})
+   
